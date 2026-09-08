3d35d366a61937f306330561fa8d8fdb7f0c9da0
ali arham <l1f21bsse0015@ucp.edu.pk>
2026-09-08 13:54:01 +0500

    fixed: added .env file and secured database url

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 
-db_url="postgresql://postgres:AliArham1321@localhost:5432/shopadmindb"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+db_url=DATABASE_URL
 engine = create_engine(db_url)
 session =sessionmaker(autocommit=False, autoflush=False, bind=engine)
